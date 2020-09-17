d68d3f9fd940b815300ef1cf226ffd495337a248
Mike Christiansen <michael.a.christiansen10@gmail.com>
2020-09-17 19:17:49 -0400

    Add project files.

---
+++ NullableExtensions.Shared/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace System.Collections.Generic
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> items) where T : class
+        {
+            foreach (var item in items)
+            {
+                if (!(item is null))
+                    yield return item;
+            }
+        }
+        public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> items) where T : struct
+        {
+            foreach (var item in items)
+            {
+                if (!(item is null))
+                    yield return item.Value;
+            }
+        }
+    }
+}
